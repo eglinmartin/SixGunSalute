4a8e1cdf6f087a459593ba54ee652d635848e4a3
MARTIN_LAPTOP\eglin <eglin.martin@gmail.com>
2024-10-02 21:55:06 +0100

    Created player class and initial item creation. Begun to populate item JSON

--- src/main.py
+++ src/main.py
@@ -2,6 +2,12 @@ import sys
 import pygame
 
 
+class Player:
+    def __init__(self):
+        self.barrel = ['ammo_brassbullet'] * 6
+        print(self.barrel)
+
+
 def run_game(screen):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -18,8 +24,9 @@ def main():
     screen = pygame.display.set_mode((screen_size['width']*screen_size['scale'],
                                       screen_size['height']*screen_size['scale']))
     pygame.display.set_caption("Six-Gun Salute")
-
     screen.fill(pygame.Color("#516c5e"))
+
+    player = Player()
 
     while True:
         run_game(screen)
